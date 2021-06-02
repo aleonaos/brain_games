5fb4d37d48a316772a6597e49f0d31c58bd31e9c
unknown <aleonaosotova@yandex.ru>
2021-06-02 12:18:38 +0300

    add eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
-
 import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
 
 const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${userName}!`);
